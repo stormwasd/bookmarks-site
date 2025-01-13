2b542e62f8e6324cc917ad4693f2c62165b2d69a
lihoujian <lihoujian@datagrand.com>
2025-01-13 16:50:37 +0800

    upd: added create_default_user.py

---
+++ management/commands/create_default_user.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3.8
+# -*- coding: utf-8 -*-
+"""
+@Description:
+@Author: lihoujian
+@Time: 2025/1/13 16:47
+@Module: create_default_user
+"""
+import os
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class Command(BaseCommand):
+    help = 'Creates a default superuser if it does not exist'
+
+    def handle(self, *args, **options):
+        username = os.getenv('DJANGO_SUPERUSER_USERNAME', 'admin')
+        email = os.getenv('DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
+        password = os.getenv('DJANGO_SUPERUSER_PASSWORD', 'adminpassword')
+
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(username=username, email=email, password=password)
+            self.stdout.write(self.style.SUCCESS('Superuser created successfully'))
+        else:
+            self.stdout.write(self.style.WARNING('Superuser already exists'))
